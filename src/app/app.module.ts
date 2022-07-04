3397270ea5bb28777eaa12677d4630c599ca0d41
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-07-05 00:37:26 +0300

    Create a login component implementing error handling features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './authentication/register/register.component';
+import { LoginComponent } from './authentication/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
+    LoginComponent,
 
   ],
   imports: [
