19c1ff76e47597b2f4aee3f111b3e5df22104c48
Dindihub <sadindi03@gmail.com>
2022-07-05 20:02:25 +0300

    Configure app components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { StaffContentFormComponent } from './staff-content-form/staff-content-form.component';
+import { CategoryComponent } from './category/category.component';
+import { SinglePostComponent } from './single-post/single-post.component';
+import { StaffPageComponent } from './staff-page/staff-page.component';
+import { StaffProfileComponent } from './staff-profile/staff-profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: StaffProfileComponent },
+  { path: 'staff-page', component: StaffPageComponent },
+  { path: 'single-post', component: SinglePostComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'staff-content-form', component: StaffContentFormComponent },
+  { path: 'category-form', component: CategoryFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
+export const routingComponents=[StaffProfileComponent,StaffPageComponent,CategoryComponent,StaffContentFormComponent,CategoryFormComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SinglePostComponent } from './single-post/single-post.component';
+import { StaffProfileComponent } from './staff-profile/staff-profile.component';
+import { StaffPageComponent } from './staff-page/staff-page.component';
+import { CategoryComponent } from './category/category.component';
+import { StaffPostComponent } from './staff-post/staff-post.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { StaffContentFormComponent } from './staff-content-form/staff-content-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinglePostComponent,
+    StaffProfileComponent,
+    StaffPageComponent,
+    routingComponents,
+    CategoryComponent,
+    StaffPostComponent,
+    CategoryFormComponent,
+    StaffContentFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
